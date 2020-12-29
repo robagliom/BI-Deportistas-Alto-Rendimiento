2970beeb676fd43a1ceefdb58548e84de0c3f427
Magdalena Robaglio <robaglio.magdalena@gmail.com>
2020-12-29 15:03:47 -0300

    Agrega login y logout

--- project/modules/auxiliar/urls.py
+++ project/modules/auxiliar/urls.py
@@ -4,4 +4,7 @@ from . import  views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('logout', views.logout_view, name='logout'),
+    path('login_template', views.login_template, name='login_template'),
+    path('login', views.login_view, name='login'),
 ]
--- project/modules/auxiliar/views.py
+++ project/modules/auxiliar/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from . import views
 
 def index(request):
-    return render(request, 'index.html')
+    usuario_logueado = False
+    usuario = None
+
+    if request.user.is_authenticated:
+        usuario_logueado = True
+        usuario = request.user.username
+
+    c = {
+            'usuario_logueado':usuario_logueado,
+            'usuario':usuario,
+         }
+    return render(request, 'index.html',context=c)
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse(views.index))
+
+def login_template(request,error=False):
+    return render(request,'login.html',context={'error':error})
+
+def login_view(request):
+    username = request.POST['documento']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect(reverse(views.index))    
+    else:
+        return login_template(request,True)
